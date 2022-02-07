c88fd8d9c1dcdac6592f3ee4fa9954e8473f8bdf
Eric Fialkowski <eric.fialkowski@gmail.com>
2022-02-07 10:32:55 -0700

    made a windows check

--- main.go
+++ main.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
+	"runtime"
 	"time"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Printf("Usage: %s <one or more files to create or update times>\n", os.Args[0])
-		os.Exit(1)
-	}
-	for _, arg := range os.Args[1:] {
-		_, err := os.Open(arg)
-		if errors.Is(err, os.ErrNotExist) {
-			// create empty file
-			_, err := os.Create(arg)
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			currentTime := time.Now().Local()
-			err := os.Chtimes(arg, currentTime, currentTime)
-			if err != nil {
-				fmt.Println(err)
-			}
+	if runtime.GOOS == "windows" {
+		if len(os.Args) == 1 {
+			fmt.Printf("Usage: %s <one or more files to create or update times>\n", os.Args[0])
+			os.Exit(1)
 		}
+		for _, arg := range os.Args[1:] {
+			_, err := os.Open(arg)
+			if os.IsNotExist(err) {
+				// create empty file
+				_, err := os.Create(arg)
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				currentTime := time.Now().Local()
+				err := os.Chtimes(arg, currentTime, currentTime)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
 
+		}
+	} else {
+		fmt.Println("You should already have a native touch command. Use it.")
+		os.Exit(2)
 	}
 }
